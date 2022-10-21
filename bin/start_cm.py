e31dc0fe017aaf74ca9e983efb7bb5b842e7337f
demjanp <peter.demjan@gmail.com>
2022-10-21 14:54:20 +0200

    fixed crash when calculating distances in windows executable version

--- bin/start_cm.py
+++ bin/start_cm.py
@@ -1,8 +1,11 @@
 from ceramatch import CMMain
+
+from multiprocessing import freeze_support
 from os import system
 import sys
 
 if __name__ == '__main__':
+	freeze_support()
 	
 	system("title CeraMatch")
 	
--- src/ceramatch/__init__.py
+++ src/ceramatch/__init__.py
@@ -1,7 +1,7 @@
-version_info = (1, 0, 41)
+version_info = (1, 0, 42)
 
 __version__ = '.'.join(map(str, version_info))
 __title__ = 'CeraMatch'
-__date__ = "20.10.2022"
+__date__ = "21.10.2022"
 
 from ceramatch.cm_main import CMMain
