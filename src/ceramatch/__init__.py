24a2a069e6b8ba97188cae1085b8f7eeda1a5668
demjanp <peter.demjan@gmail.com>
2025-04-22 13:30:24 +0200

    v1.1.3
    disabled progress bars to prevent crashing

--- src/ceramatch/__init__.py
+++ src/ceramatch/__init__.py
@@ -1,7 +1,7 @@
-version_info = (1, 1, 1)
+version_info = (1, 1, 3)
 
 __version__ = '.'.join(map(str, version_info))
 __title__ = 'CeraMatch'
-__date__ = "28.02.2025"
+__date__ = "22.04.2025"
 
 from ceramatch.cm_main import CMMain
